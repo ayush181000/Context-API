5c19d22dad348658d2259b53670d0532f1f59ec1
Ayush Garg <ayush181000@gmail.com>
2021-12-11 19:15:03 +0530

    App.js

---
+++ react-hooks-context-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+import CounterView from './views/counter-view';
+
+export default function App() {
+  return (
+    <Container>
+      <h1>React Hooks Context</h1>
+      <CounterView />
+    </Container>
+  );
+}
